cab5091670092b67111d5a91d5e97537d53825b5
Pranjal Walia <pranjalwalia77@gmail.com>
2021-02-09 01:21:18 +0530

    refactor complete, init email service

--- src/routes/Downloads.ts
+++ src/routes/Downloads.ts
@@ -1,29 +1,12 @@
 import express from 'express';
 import { downloadFile } from '../controllers/DownloadFile';
-import { File } from '../models/File';
+import { sendEmail } from '../controllers/SendEmail';
 
 const router = express.Router();
 
 router.get('/:uuid', downloadFile);
 
-router.post(
-    '/send',
-    async (
-        req: express.Request,
-        res: express.Response,
-        _next: express.NextFunction
-    ): Promise<void> => {
-        const { uuid, destination, source } = req.body;
-
-        //! validate request
-        if (!uuid || !destination || !source) {
-            //* HTTP: 422 => validation error
-            res.status(422).json({ error: 'All fields are required' });
-        }
-
-        //! query the db and fetch the file data
-        const file: any = await File.findOne({ uuid: uuid });
-    }
-);
+//* invoke email service here
+router.post('/send', sendEmail);
 
 export default router;
