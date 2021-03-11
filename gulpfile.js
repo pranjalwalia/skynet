d25528172cd977ac4c39eef12364f94ee9d027d4
Pranjal Walia <pranjalwalia77@gmail.com>
2021-03-12 01:33:47 +0530

    fix build pipeline with a hack of gulp, todo: figure out gulp.series() not working correctly

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,6 @@ const requireDir = require('require-dir');
 requireDir('./tasks', { recurse: true });
 
 gulp.task('dev:pipeline', gulp.series('dev:cleanup'));
+
+//todo: complete build script, use series
+gulp.task('build', gulp.series('buildServer'));
